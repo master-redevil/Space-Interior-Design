5f96b116069fd5553dea634bf623e6c9fa39d621
M.Arif Manzoor <muhammadarifmanzoor15@gmail.com>
2026-09-04 10:21:02 +0500

    explore div implemented

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 $(document).ready(function () {
-    $("#design-tab").mouseenter(function () { 
+    // When hovering "Design"
+    $("#design-tab").on("mouseenter", function () {
         $("#nav-bar").addClass("expanded");
-    });
-    $("#nav-bar").mouseleave(function () { 
-        $("#nav-bar").removeClass("expanded");
+        $(".dropdown-content").removeClass("active");
+        $("#design-div").addClass("active");
     });
 
-    $("#explore-tab").mouseenter(function () { 
+    // When hovering "Explore"
+    $("#explore-tab").on("mouseenter", function () {
         $("#nav-bar").addClass("expanded");
+        $(".dropdown-content").removeClass("active");
+        $("#explore-div").addClass("active");
     });
-    $("#nav-bar").mouseleave(function () { 
+
+    // When hovering non-dropdown tabs (e.g., Login, Space), close panels
+    $("#home-tab, li:has(#signup-btn), li:has(a:contains('Login'))").on("mouseenter", function () {
         $("#nav-bar").removeClass("expanded");
+        $(".dropdown-content").removeClass("active");
     });
-})
+
+    // When leaving the navbar container entirely
+    $("#nav-bar").on("mouseleave", function () {
+        $("#nav-bar").removeClass("expanded");
+        $(".dropdown-content").removeClass("active");
+    });
+});
